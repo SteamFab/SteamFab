3cabeb760ca902867ea0c111d1a8dc0aed5e340f
rupareliyagirish <rupareliya.girish@gmail.com>
2015-09-02 18:08:06 +0530

    Send grid changes - 2-9-2015
    Send grid changes - 2-9-2015

---
+++ contact-form-sendgrid-SMTP.php
@@ -0,0 +1,59 @@
+<?php
+ /*include_once "vendor/autoload.php";*/
+ /*
+  * Create the body of the message (a plain-text and an HTML version).
+  * $text is your plain-text email
+  * $html is your html version of the email
+  * If the reciever is able to view html emails then only the html
+  * email will be displayed
+  */ 
+ $text = "Hi!\nHow are you?\n";
+ $html = "<html>
+       <head></head>
+       <body>
+           <p>Hi!<br>
+               How are you?<br>
+           </p>
+       </body>
+       </html>";
+ // This is your From email address
+ $from = array('martin@caliviaconsult.com' => 'Name To Appear');
+ // Email recipients
+ $to = array(
+       'martin@caliviaconsult.com'=>'Destination 1 Name',
+       'rupareliya.girish@gmail.com'=>'Destination 2 Name'
+ );
+ // Email subject
+ $subject = 'Example PHP Email';
+
+ // Login credentials
+ $username = 'steamfab';
+ $password = 'iMNpt42NwHYPpu';
+
+ // Setup Swift mailer parameters
+ $transport = Swift_SmtpTransport::newInstance('smtp.sendgrid.net', 587);
+ $transport->setUsername($username);
+ $transport->setPassword($password);
+ $swift = Swift_Mailer::newInstance($transport);
+
+ // Create a message (subject)
+ $message = new Swift_Message($subject);
+
+ // attach the body of the email
+ $message->setFrom($from);
+ $message->setBody($html, 'text/html');
+ $message->setTo($to);
+ $message->addPart($text, 'text/plain');
+
+ // send message 
+ if ($recipients = $swift->send($message, $failures))
+ {
+     // This will let us know how many users received this message
+     echo 'Message sent out to '.$recipients.' users';
+ }
+ // something went wrong =(
+ else
+ {
+     echo "Something went wrong - ";
+     print_r($failures);
+ }
